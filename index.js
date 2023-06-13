03cadabe5ef0f3f8dc1e9d7692b45c8ec34f5bfa
mukoya <mukoya.darius@student.moringaschool.com>
2023-06-13 12:59:17 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, '+"Grace Hopper!"
+let excitedWelcomeMessage= "WELCOME TO FLATBOOK, "+"GRACE HOPPER!";
+let shortGreeting="Welcome, " + "G!"
